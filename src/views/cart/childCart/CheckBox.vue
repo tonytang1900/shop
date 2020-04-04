a8360ccbc46efc0f191c772089514a65884e2905
376905022 <376905022@163.com>
2020-04-04 12:11:02 +0800

    '第五次提交'

---
+++ src/views/cart/childCart/CheckBox.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CheckBox"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/detail/childComponent/DetailGoodsDesc.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "DetailGoodsDesc"
+    }
+</script>
+
+<style scoped>
+
+</style>
