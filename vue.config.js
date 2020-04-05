08df7990e9de798ee080375f48b18094ea0b0916
376905022 <376905022@163.com>
2020-04-05 16:23:13 +0800

    eight push

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,17 @@ module.exports = {
             },
             postcss: {
                 // 这里的选项会传递给 postcss-loader
+                plugins: [
+                    require('postcss-px-to-viewport')({
+                        'viewportWidth': 375, // (Number) The width of the viewport.
+                        'viewportHeight': 667, // (Number) The height of the viewport.
+                        'unitPrecision': 3, // (Number) The decimal numbers to allow the REM units to grow to.
+                        'viewportUnit': 'vw', // (String) Expected units.
+                        'selectorBlackList': ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+                        'minPixelValue': 1, // (Number) Set the minimum pixel value to replace.
+                        'mediaQuery': false // (Boolean) Allow px to be converted in media queries.
+                    })
+                ]
             }
         }
     }
